6aeefebab85d99fdf8a10f3661abc2a4e308c05e
yuka166@github <hoangdat1606@gmail.com>
2023-03-02 18:26:37 +0700

    Updates

--- index.js
+++ index.js
@@ -25,6 +25,35 @@ window.addEventListener("mousemove", (e) => {
     )
 })
 
+window.addEventListener("touchmove", (e) => {
+    const gallery = document.getElementById('gallery');
+
+    const mouseX = e.touches[0].clientX,
+        mouseY = e.touches[0].clientY;
+
+    const xDecimal = mouseX / window.innerWidth,
+        yDecimal = mouseY / window.innerHeight;
+
+    const xMax = gallery.offsetWidth - window.innerWidth,
+        yMax = gallery.offsetHeight - window.innerHeight;
+
+    const panX = xDecimal * xMax * -1,
+        panY = yDecimal * yMax * -1;
+
+    console.log(panX + ',' + panY)
+
+    gallery.animate(
+        {
+            transform: `translate(${panX}px, ${panY}px)`
+        },
+        {
+            duration: 800,
+            easing: 'ease-in-out',
+            fill: 'forwards'
+        }
+    )
+})
+
 function DefaultPosition() {
     const gallery = document.getElementById('gallery');
     const xMax = (gallery.offsetWidth - window.innerWidth) / 2 * -1,
